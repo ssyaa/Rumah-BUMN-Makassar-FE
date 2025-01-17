10727889ac77056e94b81ef395ab93861c4958c4
ssyaa <aulputri4877@gmail.com>
2025-01-17 14:36:41 +0800

    why page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Hero from "@/app/components/Hero";
+import Testimoni from "@/app/components/Testimoni"
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Testimoni />
     </div>
+    
   );
 }
